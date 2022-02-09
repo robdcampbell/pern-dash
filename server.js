b1904f5a2f150e7f3beb8fa89d6214c3a4909dd1
robdcampbell <rdeancampbell@gmail.com>
2022-02-09 10:58:31 -0500

    new server.js begin

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to this P.E.R.N. Stack Dashboard" });
 });
 
 require("./app/routes/tutorial.routes")(app);
